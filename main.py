c5c7d0616eacdc3684311bd191315596cb975b0e
Swarit Acharya <swarit.acharya@gmail.com>
2025-04-06 19:03:53 +0530

    Fin

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ RUN WITH THIS:
 uvicorn api:app --host 0.0.0.0 --port 8000
 """
 
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Header, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from io import BytesIO
 from fastai.learner import load_learner
 from fastai.vision.core import PILImage
 import base64
-import torch
-import torch.nn.functional as F
+import os
+from dotenv import load_dotenv
 import pathlib
 import platform
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 # Load Fastai Image Model
 MODEL_PATH = "fastai_resnet_101_model_DERMNET_gradual_10.pkl"  # Update with your model's filename
@@ -34,6 +37,11 @@ try:
     learn_txt = load_learner(TXT_MODEL_PATH)
 except Exception as e:
     raise RuntimeError(f"Failed to load text model: {e}")
+
+# API key check dependency
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Unauthorized access")
 
 # Initialize FastAPI
 app = FastAPI()
@@ -53,7 +61,7 @@ async def root():
 
 # Image Prediction Endpoint (File Upload)
 @app.post("/predict-image/")
-async def predict_image(file: UploadFile = File(...)):
+async def predict_image(file: UploadFile = File(...), auth=Depends(verify_api_key)):
     try:
         image = PILImage.create(BytesIO(await file.read()))
         pred, pred_idx, probs = learn.predict(image)
@@ -69,7 +77,7 @@ async def predict_image(file: UploadFile = File(...)):
 
 # Image Prediction Endpoint (Base64)
 @app.post("/predict-base64/")
-async def predict_base64(data: dict):
+async def predict_base64(data: dict, auth=Depends(verify_api_key)):
     try:
         if "image" not in data:
             raise HTTPException(status_code=400, detail="Missing 'image' key")
@@ -91,7 +99,7 @@ class TextInput(BaseModel):
     input_data: str
 
 @app.post("/predict-text/")
-async def predict_text(input: TextInput):
+async def predict_text(input: TextInput, auth=Depends(verify_api_key)):
     try:
         # Use the string directly, not the whole object
         pred, pred_idx, probs = learn_txt.predict(input.input_data)
